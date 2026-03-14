7cd1e8b5bb060f6eb91c05d7c264a4b0200a0026
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-03-14 12:23:58 +0100

    Implement Strategy Pattern to render page on btn-click

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
+// Les fonctions build acceptent toutes un unique paramètre HTLMElement
+// Et elles modifient le innerHTML de cet élément sans rien retourner
+
 import { buildHome } from "./home/home.js";
+import { buildMenu } from "./menu/menu.js";
+import { buildAbout } from "./about/about.js";
 
 const contentDiv = document.getElementById("content");
-buildHome(contentDiv);
+const buttons = document.querySelectorAll("button");
+const strats = {
+  "nav-btn-home": buildHome,
+  "nav-btn-menu": buildMenu,
+  "nav-btn-about": buildAbout,
+};
+
+function emptyDiv(div) {
+  div.innerHTML = "";
+}
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    emptyDiv(contentDiv);
+    strats[event.target.id](contentDiv);
+  });
+});
